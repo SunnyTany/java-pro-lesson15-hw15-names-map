dfae59c5890896d2ce301bda7c8fed9fc4faa0b3
SunnyTany <46300377+SunnyTany@users.noreply.github.com>
2026-05-22 08:27:57 +0200

    init project

---
+++ src/app/DataRepository.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataRepository {
+
+    public Map<, String> getData() {
+        Map<Integer, > map = new HashMap<>();
+
+        return map;
+    }
+}
